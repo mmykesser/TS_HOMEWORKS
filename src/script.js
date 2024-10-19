923c8ca9aa1cda54b10b8b7144a009999cafb4d4
Maksym Polinovskyi <mmykesser@gmail.com>
2024-10-19 14:29:51 +0200

    Homework #3 completed

--- src/script.js
+++ src/script.js
@@ -1,1 +1,31 @@
 'use strict';
+
+function BankAccount() {
+  let _balance = 0;
+
+  this.deposit = function (x) {
+    _balance += x;
+  };
+
+  this.withdraw = function (x) {
+    _balance -= x;
+  };
+
+  this.getBalance = function () {
+    if (_balance > 0) {
+      return `Your balance: ${_balance}$`;
+    }
+    return `Your balance is empty `;
+  };
+}
+
+const newUser = new BankAccount();
+console.log(newUser.getBalance());
+
+newUser.deposit(1591);
+console.log(newUser.getBalance());
+
+newUser.withdraw(38);
+console.log(newUser.getBalance());
+
+console.log(newUser.balance);
