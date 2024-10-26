f1b3deea7d9d96e009f6addf9b07057f60d056da
Maksym Polinovskyi <mmykesser@gmail.com>
2024-10-26 14:49:24 +0200

    Homework #5 completed

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-## Homework
+## Homework 5
+
+Напиши функцію для рекурсивного копіювання об'єкта, враховуючи вкладені об'єкти та масиви.
+
+`const original = { a: 1, b: { c: 2 }, d: [3, 4] };  
+const copy = deepClone(original);  
+// original !== copy  
+// original.b !== copy.b  
+// original.d !== copy.d`
---
+++ src/script.ts
@@ -0,0 +1,32 @@
+type deepCloneType =
+  | { [key: string]: deepCloneType }
+  | deepCloneType[]
+  | number
+  | string;
+
+function deepClone(obj: deepCloneType): deepCloneType {
+  if (obj === null || typeof obj !== 'object') {
+    return obj;
+  }
+
+  const isArray = Array.isArray(obj);
+  const clone = isArray ? [] : {};
+
+  for (const key in obj) {
+    if (obj.hasOwnProperty.call(obj, key)) {
+      (clone as { [key: string]: deepCloneType })[key] = deepClone(
+        (obj as { [key: string]: deepCloneType })[key]
+      );
+    }
+  }
+  return clone as deepCloneType;
+}
+
+const original: deepCloneType = { a: 1, b: { c: 2 }, d: [3, 4] };
+const copy: deepCloneType = deepClone(original);
+
+if (typeof copy === 'object' && copy !== null && 'b' in copy) {
+  console.log(original !== copy);
+  console.log(original.b !== (copy as { b: deepCloneType }).b);
+  console.log(original.d !== (copy as { d: deepCloneType[] }).d);
+}
